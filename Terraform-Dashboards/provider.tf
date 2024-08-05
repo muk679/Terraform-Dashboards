cf442019c2ba7b0dbf2d4b4a83eead39540bf7cd
muk679 <mukheja679@gmail.com>
2024-08-05 15:30:00 +0530

    hh

--- Terraform-Dashboards/provider.tf
+++ Terraform-Dashboards/provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "newrelic" {
   # Configuration options
   account_id = "4510907"
- // api_key = "NRAK-BCQ0CCD3QRJVPZPAZIW0WTIE35I"
+  api_key = "NRAK-BCQ0CCD3QRJVPZPAZIW0WTIE35I"
 
 }
